6fe7f1209cba54af8dc668ff87d4e9270579bb7d
willtack <will.s.tackett@gmail.com>
2021-06-02 13:36:03 -0400

    init

---
+++ code/calcSchaeferVolumes.sh
@@ -0,0 +1,3 @@
+labelDir='/home/will/Projects/healthy-t1-dataset/labels'
+fslstats -K ${labelDir}/tpl-MNI152NLin2009cAsym_res-01_atlas-Schaefer2018_desc-200Parcels17Networks_dseg.nii.gz ${labelDir}/tpl-MNI152NLin2009cAsym_res-01_atlas-Schaefer2018_desc-200Parcels17Networks_dseg.nii.gz -V
+
